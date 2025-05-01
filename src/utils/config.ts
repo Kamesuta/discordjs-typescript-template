bb316ab52e182b9fd2d4f7b0455bddce5b81e958
Kamesuta <kamesuta@gmail.com>
2025-05-01 11:10:09 +0900

    update config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
-import assert from 'assert';
-import { parse } from 'toml';
-import { getWorkdirPath } from './workdir.js';
-import { copyFileSync, existsSync, readFileSync } from 'fs';
+import assert from "assert";
+import { parse } from "toml";
+import { getWorkdirPath } from "./workdir.js";
+import { copyFileSync, existsSync, readFileSync } from "fs";
 
 /**
  * Structure of the configuration file
@@ -13,27 +13,30 @@ export interface Config {
    */
   /* eslint-disable @typescript-eslint/naming-convention */
 
-  /** Example of a string setting */
-  some_text_setting: string;
+  /** Server IDs */
+  guild_ids: string[];
 
   /* eslint-enable @typescript-eslint/naming-convention */
 }
 
 // If config.toml does not exist, copy config.default.toml
-if (!existsSync(getWorkdirPath('config.toml'))) {
+if (!existsSync(getWorkdirPath("config.toml"))) {
   copyFileSync(
-    getWorkdirPath('config.default.toml'),
-    getWorkdirPath('config.toml'),
+    getWorkdirPath("config.default.toml"),
+    getWorkdirPath("config.toml"),
   );
 }
 
 /** Configuration */
 export const config: Config = parse(
-  readFileSync(getWorkdirPath('config.toml'), 'utf-8'),
+  readFileSync(getWorkdirPath("config.toml"), "utf-8"),
 ) as Config;
 
 // Check the types
+// basic type: config.some_text_setting && typeof config.some_text_setting === 'string'
+// object type: config.some_object_setting && typeof config.some_object_setting === 'object'
+// array type: config.some_array_setting && Array.isArray(config.some_array_setting)
 assert(
-  config.some_text_setting && typeof config.some_text_setting === 'string',
-  'some_text_setting is required.',
+  config.guild_ids && Array.isArray(config.guild_ids),
+  "guild_ids is required.",
 );
